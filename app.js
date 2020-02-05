8b4364df2a95ec7550ce654a57576a261bc0493c
sahar khaled <engineer.sahar.8989@hotmail.com>
2020-02-05 21:13:21 +0200

    add

--- app.js
+++ app.js
@@ -17,28 +17,31 @@ if (person == null || person == "") {
 function greeting(){
 //input
 var age= prompt('enter your age');
-var make;
+var person;
 // processing
-if (age>0 && age<=12);
-
-{
-  make='have anice day';
-
+while (age <12){
+  console.log('you cant enter page');
+  age= prompt('enter your age again');
 }
-
   if(age>12 && age<=100) {
 
 make='im happy to visit my web site'
 
-
+return make;
 }
 
 return make;
 //output
 
 }
-
+function make_shop(){
+  var num_item = prompt('how many items you would like to make?');
+  return num_item;
+}
 var make =greeting ();
-document.write ('<h1>'+ make+'/<h1>');
+document.write ('<h1>'+ make+'</h1>');
+var num_make = make_shop();
+for(var i=0; i<num_make;i++){
+  document.write ('<h2>Item'+ (i+1) +'</h2>');
 
-
+}
